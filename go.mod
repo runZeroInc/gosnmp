a7c3073c97ad72df357663d719fbad1adb4f14b1
HD Moore <x@hdm.io>
2026-01-24 16:23:59 -0600

    add pcap-based unmarshal fuzzing

--- go.mod
+++ go.mod
@@ -11,6 +11,8 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/klauspost/compress v1.18.3 // indirect
+	github.com/klauspost/pgzip v1.2.6 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/net v0.48.0 // indirect
 	golang.org/x/sys v0.40.0 // indirect
---
+++ test_fuzz_all.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+# Some portions stolen from here:
+# https://github.com/golang/go/issues/46312#issuecomment-1727928218
+
+set -e
+
+# Fuzz time needs to include the units. For example, 60s is 60 seconds.
+fuzzTime=${1:-60s}
+
+files=$(grep -r --include='**_test.go' --files-with-matches 'func Fuzz' .)
+
+
+if [ -z "${files}" ]; then
+    fileCount=0;
+else
+    fileCount=$(echo "${files}" | wc -l | tr -d '[:space:]');
+fi
+
+
+# Generate some metrics to make output review and estimation of remaining time easier.
+testCount=0
+for file in ${files}; do
+    funcs=$(grep '^func Fuzz' "$file" | sed s/func\ // | sed 's/(.*$//')
+    for func in ${funcs}; do
+        (( testCount += 1 ))
+    done
+done
+
+echo -e "\n\n\n###########################################################################"
+echo "Running native hardware platform fuzzing"
+echo "Found a total of ${testCount} fuzz tests spread across ${fileCount} files"
+echo -e "###########################################################################\n"
+unset funcs
+unset func
+
+counter=0
+for file in ${files}; do
+        funcs=$(grep '^func Fuzz' "$file" | sed s/func\ // | sed 's/(.*$//')
+
+        for func in ${funcs}; do
+                 (( counter += 1 ))
+                echo -e "\nFuzzing ${func} in ${file} (${counter} of ${testCount})"
+                parentDir=$(dirname "$file")
+                go test -fuzz="$func" -v -fuzztime="${fuzzTime}" "$parentDir"
+        done
+done
+
+# Check for Linux on x86_64 hardware. If found, we want to add a loop to run the
+# fuzzers in 32bit as well.
+if [ "$(uname -s)" == "Linux" ]; then
+    if [ "$(uname -m)" == "x86_64" ] || [ "$(uname -m)" == "i686" ]; then
+        counter=0
+
+        echo -e "\n\n\n###########################################################################"
+        echo Running 32bit fuzzing
+        echo -e "###########################################################################\n"
+        for file in ${files}; do
+            funcs=$(grep '^func Fuzz' "$file" | sed s/func\ // | sed 's/(.*$//')
+
+            for func in ${funcs}; do
+                     (( counter += 1 ))
+                    echo -e "\n32bit fuzzing ${func} in ${file} (${counter} of ${testCount})"
+                    parentDir=$(dirname "$file")
+                    GOARCH=386 go test -v -fuzz="$func" -fuzztime="${fuzzTime}" "$parentDir"
+            done
+        done
+
+    fi
+fi
